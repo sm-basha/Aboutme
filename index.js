f7bd8071fb77c434950b28ae11a321546c7093ba
sm-basha <sm_basha@outlook.com>
2024-03-02 19:02:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,324 @@
+/**
+ * Resume/CV template created with Bootstrap 5 by @vmoratog and @jdnichollsc
+ */
+const resumeEN = {
+    photo: 'pic.png',
+    firstName: 'SHAIK ',
+    lastName: 'MAHABOOB BASHA',
+    jobTitle: 'IT Administrator',
+    city: 'Hyderabad',
+    postalCode: '500032',
+    country: 'India',
+    phone: '+91 950 2720 548',
+    email: 'sm_basha@outlook.com',
+    education: [
+      {
+        // school: 'Institución Universitaria Politécnico Grancolombiano',
+        degree: 'Quba College of Engineering',
+        // graduationDate: 'Present',
+        description: 'Computer Science Engineering'
+      },
+      {
+        // school: 'Quba college of Engg & Tech.',
+        degree: 'Graduate',
+        // graduationDate: '2013',
+        description: 'Tecnologo en Analisis y desarrollo en sistemas de información'
+      }
+    ],
+    links: [
+      {
+        label: 'GitHub Profile',
+        link: 'https://github.com/EmmanuelGaviria25'
+      },
+      {
+        label: 'LinkedIn Profile',
+        link: 'https://www.linkedin.com/in/emmanuel-gaviria-00bb16124/'
+      },
+      {
+        label: 'English certification',
+        link: 'https://www.efset.org/cert/N49DQe'
+      },
+      {
+        label: 'Youtube Channel',
+        link: 'https://www.youtube.com/channel/UC-4dSLsDkE4ItqvBFa8lGqQ'
+      }
+    ],
+    skills: [
+      'Java',
+      'JavaScript',
+      'TypeScript',
+      'Spring Boot',
+      'AngularJS and Angular11+',
+      'React and React Native',
+      'Git',
+      'Azure Devops',
+      'AWS Services',
+      'SQL',
+      'NoSQL',
+      'PL-SQL',
+      'Game Designer Environment',
+      'Unreal Engine 5',
+      'Technical leadership',
+      'Software Architecture'
+    ],
+    languages: ['English ', 'Spanish'],
+    professionalSummary: `I am a Senior software developer, having worked on a variety of projects in both backend and frontend application development. I have utilized Java, JavaScript, TypeScript, Angular, React, Android Studio in enterprise environments to develop information management systems, banking systems, e-commerce applications, and custom software solutions. My experience includes designing and implementing robust architectures using frameworks like Spring and Hibernate, as well as integrating systems through web services and RESTful APIs. Additionally, I have skills in designing relational databases and using complementary technologies such as SQL and JPA.`,
+    employmentHistory: [
+      {
+        jobTitle: 'Software Developer',
+        startDate: 'Nov 2022',
+        endDate: 'Jan 2024',
+        employer: 'Cobis',
+        city: 'Quito/Ecuador',
+        achievements: [
+              'I was developing in backend aplicación for Java, implementing API RESTFUL with Spring framework.',
+              'I developed serveral mobile aplications with JavaScript, TypeScript Android Studio and React Native.',
+              'I implemented some AWS services, used Docker, EKS, RDS, Secret manager .',
+              'I implemented continuos integration and cotinous deploymenta with Azure DevOps for application deployment and server management.'
+        ]
+      },{
+        jobTitle: 'Software Developer',
+        startDate: 'Feb 2023',
+        endDate: 'Aug 2023',
+        employer: 'Team International',
+        city: 'Medellín/Colombia',
+        achievements: [
+              'I worked as a vendor at Datacredito, developing modules for Java Springboot and JavaScrit-TypeScript Angular web applications.',
+              'I implemented some AWS services, used Docker, EKS, RDS, Secret manager .',
+              'I implemented continuos integration and cotinous deploymenta with Azure DevOps for application deployment and server management.'
+        ]
+      },
+      {
+        jobTitle: 'Senior Full-Stack Developer',
+        startDate: 'March 2022',
+        endDate: 'January 2023',
+        employer: 'Sofka Tecnologies',
+        city: 'Medellín/Colombia',
+        achievements: [
+              'I worked as a vendor at Grupo Argos, developing new modules with JavaScript-TypeScript in NodeJS, React and Angular web applications.',
+              'I implemented some AWS services and Azure DevOps topics for application deployment and server management.'
+        ]
+      },
+      {
+        jobTitle: 'Senior Full-Stack Developer',
+        startDate: 'March 2021',
+        endDate: 'March 2022',
+        employer: 'IAS Software',
+        city: 'Medellín/Colombia',
+        achievements: [
+              'I worked as a vendor at Bancolombia, developing new modules for Java SpringBoot and JavaScript-TypeScript in Angular web applications.',
+              'I implemented some AWS services, used Docker, EKS, RDS, Secret manager .',
+              'I implemented continuos integration and cotinous deploymenta with Azure DevOps for application deployment and server management.'
+        ]
+      },
+      {
+        jobTitle: 'Senior Developer',
+        startDate: 'Aug 2020',
+        endDate: 'March 2021',
+        employer: 'Matrixtech',
+        city: 'Medellín/Colombia',
+        achievements: [
+          'I leaded a team of 3 developers and coordinated with other teams to build a web application with Java SpringBoot and JavaScript-TypeScript in Angular.',
+          'I developed the SuperChance module for the GANA application (https://www.gana.com.co/) using the JavaScript-TypeScript in React language as frontend and Java EE implementing plugins.',
+          'I developed the Raspita module for the GANA aplication web and mobile using the AndroidStudio with Java language as frontend and Java EE implementing plugins.'
+        ]
+      }
+    ],
+  }
+  
+  const resumeES = {
+     photo: 'https://awsbucket-emmanuelgaviria.s3.amazonaws.com/profile.jpeg',
+     firstName: 'Juan Emmanuel',
+     lastName: 'Gaviria López',
+     jobTitle: 'Desarrollador Full-Stack ',
+     city: 'Medellin',
+     postalCode: '051052',
+     country: 'Colombia',
+     phone: '+57 313 602 9181',
+     email: 'emmanuel.gaviria.25@gmail.com',
+     education: [
+       {
+         school: 'Institución Universitaria Politécnico Grancolombiano',
+         degree: 'En proceso',
+         graduationDate: 'Actualmente',
+         description: 'Ingenieria de Software'
+       },
+       {
+         school: 'SENA: Servicio Nacional de Aprendizaje',
+         degree: 'Graduado',
+         graduationDate: '2016',
+         description: 'Tecnologo en Analisis y desarrollo en sistemas de información'
+       }
+     ],
+     links: [
+       {
+         label: 'Perfil GitHub',
+         link: 'https://github.com/EmmanuelGav'
+       },
+       {
+         label: 'Perfil LinkedIn',
+         link: 'https://www.linkedin.com/in/emmanuel-gaviria-00bb16124/'
+       },
+       {
+         label: 'Certificado de Ingles',
+         link: 'https://www.efset.org/cert/N49DQe'
+       },
+       {
+         label: 'Canal de Youtube',
+         link: 'https://www.youtube.com/channel/UC-4dSLsDkE4ItqvBFa8lGqQ'
+       }
+     ],
+     skills: [
+       'Java',
+       'Spring Boot',
+       'JavaScript',
+       'TypeScript',
+       'Angular',
+       'ReactJs',
+       'NextJs',
+       'Azure',
+       'Devops',
+       'Servicios AWS',
+       'PostgreSQL',
+       'Oracle-PL SQL',
+       'MySQL',
+       'Desarrollo de juegos',
+       'Unreal Engine 5'
+     ],
+     languages: ['Ingles', 'Español'],
+     professionalSummary: `Soy un desarrollador de software fullstack senior y he trabajado en una variedad de proyectos en el desarrollo de aplicaciones tanto backend como frontend. He utilizado JavaScript, TypeScript Angular, React, Java en entornos empresariales para desarrollar sistemas de gestión de información, sistemas bancarios, aplicaciones de comercio electrónico y soluciones de software personalizadas. Mi experiencia incluye diseñar e implementar arquitecturas robustas utilizando frameworks como NextJS, Spring e Hibernate, así como la integración de sistemas a través de servicios web y API RESTful.`,
+     employmentHistory: [
+      {
+        jobTitle: 'Software Developer',
+        startDate: 'Noviembre 2022',
+        endDate: 'Enero 2024',
+        employer: 'Cobis',
+        city: 'Quito/Ecuador',
+        achievements: [
+              'Estuve desarrollando varias aplicaciones web con React y NextJs. de un sistema de gestión de cliente desde la web',
+              'Estuve desarrollando una aplicación backend para Java, implementando API RESTFUL con Spring framework.',
+              'Implementé la integración continua y la implementación continua con Azure DevOps para la implementación de aplicaciones y la administración de servidores.'
+  
+        ]
+      }, {
+        jobTitle: 'Software Developer',
+        startDate: 'Febrero 2023',
+        endDate: 'Agosto 2023',
+        employer: 'Team International',
+        city: 'Medellín/Colombia',
+        achievements: [
+              'Trabajé como proveedor en Datacredito, desarrollando módulos para aplicaciones web Java Springboot y Angular.',
+              'Implementé algunos servicios de AWS, usé Docker, EKS, RDS, Secret Manager.',
+              'Implementé la integración continua y la implementación continua con Azure DevOps para la implementación de aplicaciones y la administración de servidores.'
+        ]
+      },
+       {
+         jobTitle: 'Desarrollador Full-Stack Senior',
+         startDate: 'Marzo 2022',
+         endDate: 'Enero 2023',
+         employer: 'Sofka Tecnologies',
+         city: 'Medellín/Colombia',
+         achievements: [
+           'Trabajé como proveedor en Grupo Argos, desarrollando nuevos módulos para NodeJS y aplicaciones Angular.',
+           'Utilizé algunos servicios de AWS y temas de Azure DevOps para el despliegue de aplicativos y manejo de servidores.'
+         ]
+       },
+       {
+         jobTitle: 'Desarrollador Full-Stack Senior',
+         startDate: 'Marzo 2021',
+         endDate: 'Marzo 2022',
+         employer: 'IAS Software',
+         city: 'Medellín/Colombia',
+         achievements: [
+           'Trabajé como proveedor en Bancolombia, desarrollando nuevos módulos para Spring Boot y aplicaciones Angular.',
+           'Implementé algunos servicios de AWS y temas de Azure DevOps para el despliegue de aplicativos y manejo de servidores.'
+         ]
+       },
+       {
+         jobTitle: 'Desarrollador Senior ',
+         startDate: 'Agosto 2020',
+         endDate: 'Marzo 2021',
+         employer: 'Matrixtech',
+         city: 'Medellín/Colombia',
+         achievements: [
+           'Me uní para trabajar con el equipo de desarrollo a cargo de las aplicaciones cliente de GANA.',
+           'Dirigí un equipo de 3 desarrolladores y coordinó con otros equipos para crear una aplicación web con SpringBoot y Angular.',
+           'Desarrollé el módulo SuperChance para la aplicación GANA (https://www.gana.com.co/) utilizando el lenguaje React como interfaz y complementos de implementación de Java EE.',
+           'Desarrollé el módulo Raspita para la aplicación web y móvil de GANA usando AndroidStudio con lenguaje Java como frontend e implementando plugins Java EE.'
+         ]
+       },
+       {
+         jobTitle: 'Analista Desarrollador TI 2',
+         startDate: 'Mayo 2018',
+         endDate: 'Agosto 2020',
+         employer: 'Konecta Fabrica de Software',
+         city: 'Medellín/Colombia',
+         achievements: [
+           'Me uní para trabajar con el equipo de desarrollo encargado de mejorar la gestión de procesos de negocio de las aplicaciones de Konecta.',
+           'Fuí el primer empleado en implementar el tecnologia Angular en la empresa y encargado de capacitar al equipo para comenzar a crear o implementar aplicaciones web con Angular 8+.',
+           'Implementé desarrollos utilizando el lenguaje JQuery como frontend y Spring como backend.',
+           'Modifique el diseño de una aplicación que estaba en la versión anterior ExtJS de Bootstrap.',
+         ]
+       }
+     ],
+  }
+  
+  const lang = "EN";
+  
+  const formatResume = (r, l) => ({
+    resume: {
+      ...r,
+      address: [
+        r.country,
+        r.city,
+        r.postalCode
+      ].filter(Boolean).join(', '),
+      lang: l
+    }
+  });
+  
+  var vm = new Vue({
+    el: "#app",
+    data: {
+      resume: {
+        ...resumeEN,
+        address: [
+          resumeEN.country,
+          resumeEN.city,
+          resumeEN.postalCode
+        ].filter(Boolean).join(', '),
+        lang: lang
+      }
+    },
+    methods: {
+      changeLang: function(lang) {
+        this.resume = lang=="ES" ?{
+          ...resumeES,
+          address: [
+            resumeES.country,
+            resumeES.city,
+            resumeES.postalCode
+          ].filter(Boolean).join(', '),
+          lang: lang
+        }: {
+          ...resumeEN,
+          address: [
+            resumeEN.country,
+            resumeEN.city,
+            resumeEN.postalCode
+          ].filter(Boolean).join(', '),
+          lang: lang
+        }
+      }
+    }
+  });
+  
+  /**
+   * Wait for animatable-component to be loaded (Only for VanillaJS)
+   **/
+  function animatableLoaded() {
+    document.querySelector('body').classList.remove('d-none');
+  }
+  if (customElements) {
+    customElements.whenDefined('animatable-component').then(animatableLoaded);
+  } else animatableLoaded()
